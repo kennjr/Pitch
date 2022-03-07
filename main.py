854adb7e58c7939d5300c1fa3b1c889a58799565
kennjr <ken.juniour@student.moringaschool.com>
2022-03-07 10:53:11 +0300

    Added the test flask code for the app

--- main.py
+++ main.py
@@ -4,13 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello_wrld():
+    return "Hello world"
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
