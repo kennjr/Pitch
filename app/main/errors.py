61dd575ef9c9104205b23c874e4678765dd5f88a
kennjr <ken.juniour@student.moringaschool.com>
2022-03-09 22:26:59 +0300

    Added code for the error page and styled it accordingly

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,17 +1,16 @@
 from flask import render_template
-from app import app
 from urllib import error
 
 
 # @app.errorhandler(404)
+from app.main import main
 
 
-
-@app.errorhandler(404)
+@main.app_errorhandler(404)
 def page_not_found(error):
     return render_template('404_page.html'), 404
 
 
-@app.errorhandler(error.HTTPError)
+@main.app_errorhandler(error.HTTPError)
 def http_error(error):
     return render_template('404_page.html'), 404
