8dd7af6a71d67777110594a3b0679a3d3ab117c3
kennjr <ken.juniour@student.moringaschool.com>
2022-03-10 10:55:14 +0300

    Added the Photo model class and made a connection btwn it and the user class

--- app/models.py
+++ app/models.py
@@ -17,3 +17,17 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+    photos = db.relationship('Photo', backref='photo_url', lazy="dynamic")
+
+
+class Photo(db.Model):
+    __tableanme__ = "photos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    type = db.Column(db.String(255))
+    photo_url = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Photo {self.type}'
+
