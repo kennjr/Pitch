4dde4a216f838c2843a430445ed8faca46e047a1
kennjr <ken.juniour@student.moringaschool.com>
2022-03-11 02:30:56 +0300

    Added the db migration to the app instance

--- main.py
+++ main.py
@@ -5,18 +5,21 @@
 from flask_script import Manager
 
 from app import create_app, db
-from app.models import User
+from app.models import User, Photo
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app("development")
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_ctx():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Photo=Photo)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # manager.run()
-    app.run()
+    manager.run()
+    # app.run()
